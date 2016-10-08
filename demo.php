66fd285828cf99195bfb50b1a635dea891f98e19
Nina Herrmann <n_herr03@uni-muenster.de>
2016-10-08 14:22:52 +0200

    eine Aenderung

---
+++ demo.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: nina
+ * Date: 08.10.16
+ * Time: 14:21
+ */
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head> <!--Informationen über die Webside-->
+    <meta charset="utf-8">
+    <titel>Webseite zur Demo</titel>
+    <meta name="Demo" content="Demo Seite">
+</head>
+<body>
+<div id="wrapper">
+    <header id="kopfbereich">
+        <h1>Unsere Demo Seite</h1>
+    </header>
+
+    <main id="inhaltsbereich">
+
+    </main>
+    <footer id="Fußbereich">
+
+    </footer>
+</div>
+</body>
+</html>
