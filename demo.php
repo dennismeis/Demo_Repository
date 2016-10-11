32938012ee0455bb5369eb8d40743be7099a88dd
Dennis Meis <d_meis07@uni-muenster.de>
2016-10-11 14:59:51 +0200

    Dennis neuer Inhaltsbereich

--- demo.php
+++ demo.php
@@ -19,7 +19,7 @@
         <h1>Unsere Demo Seite</h1>
     </header>
 
-    <main id="Ninasneuer inhaltsbereich">
+    <main id="Dennis neuer inhaltsbereich">
     </main>
     <footer id="Fußbereich">
 
